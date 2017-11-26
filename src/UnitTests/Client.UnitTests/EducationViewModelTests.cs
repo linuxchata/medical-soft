4e1d665fef05a24a2f2cdc43590d9e6dfb2ee786
LinuxChata <linuxchata@hotmail.com>
2017-11-26 16:52:42 +0100

    Add unit test project for Client

---
+++ src/UnitTests/Client.UnitTests/EducationViewModelTests.cs
@@ -0,0 +1,56 @@
+﻿using Client.Cache.Interface;
+using Client.Providers;
+using Client.ViewModel;
+using Common.Builder;
+using DataAccess;
+using Moq;
+using NUnit.Framework;
+
+namespace Client.UnitTests
+{
+    [TestFixture]
+    public class EducationViewModelTests
+    {
+        private Mock<IUnitOfWork> unitOfWorkMock;
+
+        private Mock<IViewModelBuilder> viewModelBuilderMock;
+
+        private Mock<IViewBuilder> viewBuilderMock;
+
+        private Mock<IMessageBoxProvider> messageBoxProviderMock;
+
+        private Mock<IEducationCache> educationCacheMock;
+
+        public void Init()
+        {
+            this.unitOfWorkMock = new Mock<IUnitOfWork>();
+            this.viewModelBuilderMock = new Mock<IViewModelBuilder>();
+            this.viewBuilderMock = new Mock<IViewBuilder>();
+            this.messageBoxProviderMock = new Mock<IMessageBoxProvider>();
+            this.educationCacheMock = new Mock<IEducationCache>();
+        }
+
+        [Test]
+        public void CtorEducationViewModel_ShouldReturnNotNull_Test()
+        {
+            // Arrange
+            // Act
+            var viewModel = this.CreateEducationViewModel();
+
+            // Assert
+            Assert.That(viewModel, Is.Not.Null);
+        }
+
+        private EducationViewModel CreateEducationViewModel()
+        {
+            var viewModel = new EducationViewModel(
+                unitOfWorkMock.Object,
+                viewModelBuilderMock.Object,
+                viewBuilderMock.Object,
+                messageBoxProviderMock.Object,
+                educationCacheMock.Object);
+
+            return viewModel;
+        }
+    }
+}
